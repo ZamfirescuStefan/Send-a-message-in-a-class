928a9369df2af4eda8691d23d18bb6f87750f036
stefanzamfirescu <zamfirescu.stefan12@gmail.com>
2021-05-28 17:14:53 +0300

    complete version

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from math import sqrt
 import sys
 import time
 
-""" 
-    In program:
-        -> x reprezinta linia 
-        -> y reprezinta coloana 
-"""
-
 
 def citire_date(fin):
     lista_elevi = []
@@ -55,6 +49,13 @@ def afisare_date(lista_elevi, elevi_suparati, elevi_ascultati, emitator, recepto
     fout.write("Emitatorul mesajului este {} iar receptorul este {} ".format(emitator, receptor))
 
 
+""" 
+    In program:
+        -> x reprezinta linia 
+        -> y reprezinta coloana 
+"""
+
+
 class NodParcurgere:
     graf = None  # static
     '''
@@ -87,13 +88,17 @@ class NodParcurgere:
 
         outputString += "\n"
         fout.write(outputString)
+        fout.write(f"Lungime: {len(l)}\n")
         fout.write("Cost: {}".format(self.g))
         return len(l)
 
     def contineInDrum(self, infoNodNou):
         nodDrum = self
         while nodDrum is not None:
-            if infoNodNou == nodDrum.info[:-1]:
+            # pentru a vedea daca doua stari sunt la fel luam in
+            # considerare doar pozitia biletului si a profesorului
+            # nu si vecinul de la care a venit sau directia din care vine
+            if infoNodNou[:-2] == nodDrum.info[:-2]:
                 return True
             nodDrum = nodDrum.parinte
 
@@ -117,6 +122,20 @@ class Graph:  # graful problemei
         #  lista de elevi pe care profesorul ii poate vedea daca trimite biletul
         self.elevi_inaccesibili = []
 
+    def euristica_complexa (self, x, y):
+        rand_nod_final, coloana_nod_final = self.find_elev(self.receptor)
+        linia_optima = len(self.lista_elevi) - 1 if x == len(self.lista_elevi) - 1 else len(self.lista_elevi) - 2
+        euristica_val = 0
+        if (coloana_nod_final == y) or (y % 2 == 0 and coloana_nod_final == y + 1) or (y % 2 == 1 and coloana_nod_final == y - 1):
+            euristica_val = abs(x - rand_nod_final)
+        elif x == len(self.lista_elevi) - 1 and rand_nod_final == len(self.lista_elevi) - 1:
+            euristica_val = abs(y - coloana_nod_final)
+        else:
+            euristica_val = abs(x - linia_optima) + abs(y - coloana_nod_final) + abs(x - linia_optima)
+
+        # fout.write(f"Euristica de la elem {x}, {y} la nodul final este {euristica_val}\n")
+        return  euristica_val
+
     def calculeaza_euristica(self, x, y):
         """
         calculeaza euristica pentru un nod pozitionat la coordonatele x, y
@@ -128,10 +147,17 @@ class Graph:  # graful problemei
             rand_nod_final, coloana_nod_final = self.find_elev(self.receptor)
             return abs(x - rand_nod_final) + abs(y - coloana_nod_final)
         elif self.tip_euristica == 2:  # euclidian distance
-            rand_nod_final, coloana_nod_final = self.find_elev(self.receptor)
-            return sqrt((x - rand_nod_final) ** 2 + (y - coloana_nod_final) ** 2)
+            return self.euristica_complexa(x, y)
+        elif self.tip_euristica == 3:
+            if x > len(self.lista_elevi) / 2:
+                return x - 1  # euristica neadmisibila
+            else:
+                return x + 1
         else:
-            return 1  # banal euristic
+            if self.find_elev(self.receptor) != (x, y):
+                return 1  # banal euristic
+            else:
+                return 0
 
     def testeaza_scop(self, nodCurent):
         return nodCurent.info[0] == self.find_elev(self.receptor)
@@ -197,13 +223,27 @@ class Graph:  # graful problemei
         x_elev, y_elev = nodCurent.info[0]
         student_actual = self.lista_elevi[x_elev][y_elev]
         x, y = self.find_elev(vecin)
-        infomationNode = [(x, y), info_prof, vecin, afis_chr]
+        nodeInformation = [(x, y), info_prof, vecin, afis_chr]
         if self.sunt_certati(student_actual, vecin) is False and vecin not in elevi_inaccesibili and \
-                not nodCurent.contineInDrum(infomationNode[:-1]):
-            cost = nodCurent.g + 1
+                not nodCurent.contineInDrum(nodeInformation):
+
+            cost = self.calculeaza_cost(nodCurent, vecin)
+            # cost = nodCurent.g + 1
             h = self.calculeaza_euristica(x, y)
-            nodNou = NodParcurgere(infomationNode, nodCurent, cost, h)
+            nodNou = NodParcurgere(nodeInformation, nodCurent, cost, h)
             listaSuccesori.append(nodNou)
+
+    def calculeaza_cost(self, nodCurent, vecin):
+        cost = nodCurent.g
+        x_nodCurent, y_nodCurent = nodCurent.info[0]
+        x_vecin, y_vecin = self.find_elev(vecin)
+        if (y_nodCurent % 2 == 0 and y_vecin == y_nodCurent - 1) or \
+           (y_nodCurent % 2 == 1 and y_vecin == y_nodCurent + 1):
+            cost += 2
+        elif abs(x_nodCurent - x_vecin) == 1:
+            cost += 1
+        # fout.write(f"Costul de la {x_nodCurent}, {y_nodCurent} la {x_vecin}, {y_vecin} este {cost}\n")
+        return cost
 
     def sunt_certati(self, nume_elev1, nume_elev2):
         """
@@ -296,46 +336,98 @@ class Graph:  # graful problemei
         return sir
 
 
+def a_star_optimizat(gr, timeout):
+    l_open = [NodParcurgere([gr.find_elev(gr.emitator), [gr.find_elev(gr.elevi_ascultati[0]), 0, 0], \
+                               gr.emitator], None, 0, 1)]
+    # l_closed contine nodurile expandate
+    l_closed = []
+    start_time = time.time()
+    nr_max_noduri = 0
+    nr_total_noduri = 0
+    while len(l_open) > 0:
+        nodCurent = l_open.pop(0)
+        actual_time = time.time()
+        l_closed.append(nodCurent)
+        if timeout != -1:
+            if actual_time - start_program_time >= timeout:
+                fout.write("Limita de timp a fost deparita: \n")
+                return
+        if gr.testeaza_scop(nodCurent):
+            fout.write("Solutie: ")
+            nodCurent.afisDrum()
+            fout.write("\nTimpul de gesire al solutiei: {}s\n".format(actual_time - start_time))
+            fout.write(f"Numarul maxim de noduri la un moment dat este: {nr_max_noduri}\n")
+            fout.write(f"Numarul total de noduri este: {nr_total_noduri}")
+            fout.write("\n---------------------------------------------------------\n\n")
+            return
+
+        lSuccesori = gr.genereazaSuccesori(nodCurent)
+        nr_total_noduri += len(lSuccesori)
+        for s in lSuccesori:
+            gasitC = False
+            for nodC in l_open:
+                if s.info == nodC.info:
+                    gasitC = True
+                    if s.f >= nodC.f:
+                        lSuccesori.remove(s)
+                    else:  # s.f<nodC.f
+                        l_open.remove(nodC)
+                    break
+            if not gasitC:
+                for nodC in l_closed:
+                    if s.info == nodC.info:
+                        if s.f >= nodC.f:
+                            lSuccesori.remove(s)
+                        else:  # s.f<nodC.f
+                            l_closed.remove(nodC)
+                        break
+        for s in lSuccesori:
+            i = 0
+            gasit_loc = False
+            for i in range(len(l_open)):
+                if l_open[i].f > s.f or (l_open[i].f == s.f and l_open[i].g <= s.g):
+                    gasit_loc = True
+                    break
+            if gasit_loc:
+                l_open.insert(i, s)
+            else:
+                l_open.append(s)
+        nr_max_noduri = max(nr_max_noduri, len(l_open))
+
+
 def a_star(gr, nrSolutiiCautate, timeout):
-    # in coada vom avea doar noduri de tip NodParcurgere (nodurile din arborele de parcurgere)
     x, y = gr.find_elev(gr.emitator)
     nod_start = NodParcurgere([gr.find_elev(gr.emitator), [gr.find_elev(gr.elevi_ascultati[0]), 0, 0], \
                                gr.emitator], None, 0, 1)
     index_sol = 1
     c = [nod_start]
     start_time = time.time()
+    nr_max_noduri = 0
+    nr_total_noduri = 0
     while len(c) > 0:
         nodCurent = c.pop(0)
-        # fout.write("Nodul {} are urmatorul drum: \n".format(nodCurent.info[0]))
-        # nodCurent.afisDrum()
-        # fout.write("\n")
         actual_time = time.time()
         if timeout != -1:
             if actual_time - start_program_time >= timeout:
-                fout.write("Limita de timp a fost deparita: \n")
-                exit(1)
+                fout.write("Limita de timp a fost deparita: \n\n")
+                return
 
         if gr.testeaza_scop(nodCurent):
             global has_solution
             has_solution = True
             fout.write(f"Solutie {index_sol}: \n")
             nodCurent.afisDrum()
-            fout.write("\nTimpul de gasire al solutiei: {}".format(actual_time - start_time))
-            fout.write("\n----------------\n\n")
+            fout.write("\nTimpul de gasire al solutiei: {}s\n".format(actual_time - start_time))
+            fout.write(f"Numarul maxim de noduri la un moment dat este: {nr_max_noduri}\n")
+            fout.write(f"Numarul total de noduri este: {nr_total_noduri}")
+            fout.write("\n---------------------------------------------------------\n\n")
             nrSolutiiCautate -= 1
             index_sol += 1
             if nrSolutiiCautate == 0:
                 return
 
+        # fout.write(f"Nod curent {nodCurent.info[0]} are g = {nodCurent.g} si h = {nodCurent.h}\n")
         lSuccesori = gr.genereazaSuccesori(nodCurent)
-
-        # Debug
-        # fout.write("nodul {} are urmatorii succesori: \n".format(nodCurent))
-        # for elem in lSuccesori:
-        #     elem_sir = str(elem.info)
-        #     fout.write(elem_sir)
-        # fout.write("\n")
-        # End debug
 
         for s in lSuccesori:
             i = 0
@@ -348,32 +440,34 @@ def a_star(gr, nrSolutiiCautate, timeout):
                 c.insert(i, s)
             else:
                 c.append(s)
+        nr_total_noduri += len(lSuccesori)
+        nr_max_noduri = max(nr_max_noduri, len(c))
 
 
 def ucs(gr, nrSolutiiCautate, timeout):
-    # in coada vom avea doar noduri de tip NodParcurgere (nodurile din arborele de parcurgere)
     nod_start = NodParcurgere([gr.find_elev(gr.emitator), [gr.find_elev(gr.elevi_ascultati[0]), 0, 0], \
                                gr.emitator], None, 0, 1)
     index_sol = 1
     c = [nod_start]
     start_time = time.time()
+    nr_max_noduri = 0
+    nr_total_noduri = 0
     while len(c) > 0:
         nodCurent = c.pop(0)
-        # fout.write("Nodul {} are urmatorul drum: \n".format(nodCurent.info[0]))
-        # nodCurent.afisDrum()
-        # fout.write("\n")
         acutual_time = time.time()
         if timeout != -1:
             if acutual_time - start_program_time >= timeout:
                 fout.write("Limita de timp a fost deparita: \n")
-                exit(1)
+                return
         if gr.testeaza_scop(nodCurent):
             global has_solution
             has_solution = True
             fout.write(f"Solutie {index_sol}: \n")
             nodCurent.afisDrum()
-            fout.write("\nTimpul de gesire al solutiei: {}".format(acutual_time - start_time))
-            fout.write("\n----------------\n\n")
+            fout.write("\nTimpul de gesire al solutiei: {}s\n".format(acutual_time - start_time))
+            fout.write(f"Numarul maxim de noduri la un moment dat este: {nr_max_noduri}\n")
+            fout.write(f"Numarul total de noduri este: {nr_total_noduri}")
+            fout.write("\n---------------------------------------------------------\n\n")
             nrSolutiiCautate -= 1
             index_sol += 1
             if nrSolutiiCautate == 0:
@@ -392,6 +486,56 @@ def ucs(gr, nrSolutiiCautate, timeout):
                 c.insert(i, s)
             else:
                 c.append(s)
+
+        nr_max_noduri = max(nr_max_noduri, len(c))
+        nr_total_noduri += len(lSuccesori)
+
+
+def ida_star(gr, nsol):
+    nodStart = NodParcurgere([gr.find_elev(gr.emitator), [gr.find_elev(gr.elevi_ascultati[0]), 0, 0], \
+                               gr.emitator], None, 0, 1)
+    limita = nodStart.f
+    actual_time = time.time()
+    while True:
+
+        # fout.write("Limita de pornire: " + str(limita))
+        if timeout != -1:
+            if actual_time - start_program_time >= timeout:
+                fout.write("Limita de timp a fost deparita: \n")
+                return
+
+        nsol, rez = construieste_drum(gr, nodStart, limita, nsol)
+        if rez == "gata":
+            break
+        if rez == float('inf'):
+            # fout.write("Nu exista solutii!")
+            break
+        limita = rez
+        # fout.write(">>> Limita noua: " + str(limita))
+
+
+def construieste_drum(gr, nodCurent, limita, nsol):
+    # fout.write("A ajuns la: " + str(nodCurent) + "\n")
+    if nodCurent.f > limita:
+        return nsol, nodCurent.f
+    if gr.testeaza_scop(nodCurent) and nodCurent.f == limita:
+        fout.write("Solutie: \n")
+        nodCurent.afisDrum()
+        fout.write("\n----------------\n")
+        nsol -= 1
+        if nsol == 0:
+            return 0, "gata"
+    lSuccesori = gr.genereazaSuccesori(nodCurent)
+    minim = float('inf')
+    for s in lSuccesori:
+        nsol, rez = construieste_drum(gr, s, limita, nsol)
+        if rez == "gata":
+            return 0, "gata"
+        # fout.write("Compara " + str(rez) + " cu " + str(minim) + "\n")
+        if rez < minim:
+            minim = rez
+            # fout.write("Noul minim: " + str(minim) + "\n")
+    return nsol, minim
 
 
 def check_args():
@@ -417,8 +561,8 @@ def check_args():
 def make_files(dir_in, dir_out):
     try:
         listaFisiere = os.listdir(dir_in)
-    except Exception as eroare:
-        print("Path to test1 file is invalid, exiting...")
+    except Exception as _:
+        print("Path to test4 file is invalid, exiting...")
         quit()
     if not os.path.exists(dir_out):
         os.mkdir(dir_out)
@@ -446,17 +590,19 @@ def check_input(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, recep
     if is_emitator_exists is False or is_receptor_exists is False:
         fout.write("Emitatorul sau repectorul nu exita, verificati inputul sau " +
                    "unul din acesti colegi lipsesc ")
-        exit(0)
+        return
 
 
 def main():
+    global timeout
     dir_in, dir_out, nsol, timeout = check_args()
     input_files, output_files = make_files(dir_in, dir_out)
 
-    tip_euristica = int(input("Alege tipurl de euristica: \n" +
-                              "1) Manhatten \n" +
-                              "2) Euclidiana \n" +
-                              "3) Banala \n"))
+    # tip_euristica = int(input("Alege tipurl de euristica: \n" +
+    #                           "1) Manhatten \n" +
+    #                           "2) Euristica complexa \n" +
+    #                           "3) Neadmisibila \n" +
+    #                           "4) Banala \n"))
 
     for index in range(len(input_files)):
         fin = open(input_files[index], 'r')
@@ -466,12 +612,60 @@ def main():
 
         lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor = citire_date(fin)
         check_input(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor)
-        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, tip_euristica)
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 2)
 
         global has_solution
         has_solution = False
         # daca timeout-ul este -1 programul se va rula fara timeout
+        fout.write("          ----- A star euristica 1 -----\n")
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 1)
+        global start_program_time
+
+        start_program_time = time.time()
         a_star(gr, nsol, timeout)
+
+        fout.write("\n          ----- A star euristica 2 -----\n")
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 2)
+        start_program_time = time.time()
+        a_star(gr, nsol, timeout)
+
+        fout.write("\n          ----- A star euristica neadmisibila -----\n")
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 3)
+        start_program_time = time.time()
+        a_star(gr, nsol, timeout)
+
+        fout.write("\n          ----- A star euristica banala -----\n")
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 4)
+        start_program_time = time.time()
+        a_star(gr, nsol, timeout)
+
+        fout.write("\n          ----- A star optimizat euristica 1 -----\n")
+        start_program_time = time.time()
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 1)
+        a_star_optimizat(gr, timeout)
+
+        fout.write("\n          ----- A star optimizat euristica 2-----\n")
+        start_program_time = time.time()
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 2)
+        a_star_optimizat(gr, timeout)
+
+        fout.write("\n          ----- A star optimizat euristica neadmisibila-----\n")
+        start_program_time = time.time()
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 3)
+        a_star_optimizat(gr, timeout)
+
+        fout.write("\n          ----- A star optimizat euristica banala -----\n")
+        start_program_time = time.time()
+        gr = Graph(lista_elevi, elevi_suparati, M, elevi_ascultati, emitator, receptor, 4)
+        a_star_optimizat(gr, timeout)
+
+        fout.write("\n          ----- UCS -----\n")
+        start_program_time = time.time()
+        ucs(gr, nsol, timeout)
+
+        fout.write("\n          ----- IDA star -----\n")
+        ida_star(gr, nsol)
+
         if has_solution is False:
             fout.write("This input has no solutions .... :(")
         fin.close()
@@ -479,5 +673,4 @@ def main():
 
 
 if __name__ == '__main__':
-    start_program_time = time.time()
     main()
